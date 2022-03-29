ce1f6713c251676dfb9f8c82a68cb4b18529523a
Marta Torres Burguillo <99444191+burguimarta@users.noreply.github.com>
2022-03-29 09:31:02 +0200

    finish html and variables js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let numbers = document.querySelector (".js-numbers")
+let betNumber = document.querySelector (".js-betNumber")
+let money = document.querySelector ("js-money")
+
+document.querySelector (".js-button")
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+   }
